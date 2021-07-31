e2e804643557eacfe6ce65076d242e6d8bacba0b
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-07-31 10:11:54 -0400

    add icons

---
+++ src/Icons/index.js
@@ -0,0 +1,36 @@
+// export { default as airplay } from "./airplay.svg";
+// export { default as codepen } from "./codepen.svg";
+
+import airplay from "./airplay.svg";
+import codepen from "./codepen.svg";
+import codesandbox from "./codesandbox.svg";
+import coffee from "./coffee.svg";
+import github from "./github.svg";
+import gitlab from "./gitlab.svg";
+import headphones from "./headphones.svg";
+import linkedin from "./linkedin.svg";
+import mail from "./mail.svg";
+import map from "./map.svg";
+import phone from "./phone.svg";
+import trello from "./trello.svg";
+import twitch from "./twitch.svg";
+import twitter from "./twitter.svg";
+
+const iconsArray = [
+  airplay,
+  codepen,
+  codesandbox,
+  coffee,
+  github,
+  gitlab,
+  headphones,
+  linkedin,
+  mail,
+  map,
+  phone,
+  trello,
+  twitch,
+  twitter
+];
+
+export default iconsArray;
