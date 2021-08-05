fd8bb3c7f8df62f7e92c9878d99119432fd2c75b
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-08-05 08:51:58 -0400

    add utils folder and move moveViewPortToCenter

---
+++ src/Utils/index.js
@@ -0,0 +1,28 @@
+/**
+ * @description Move the viewport to the center of the page using an interval
+ * @param {PointerEvent}
+ */
+const moveViewPortToCenter = (event) => {
+  return new Promise((resolve) => {
+    const pageWrapperSelector = () => document.querySelector("section");
+    let currentPos = event.pageY;
+    const point = Math.ceil(
+      document.body.clientHeight - pageWrapperSelector().clientHeight
+    );
+    const moveToCenterInverval = setInterval(() => {
+      console.log("centering");
+      if (currentPos >= point) {
+        resolve(currentPos);
+        clearInterval(moveToCenterInverval);
+      }
+      pageWrapperSelector().scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+        inline: "center"
+      });
+      currentPos += 60;
+    }, 10);
+  });
+};
+
+export { moveViewPortToCenter };
