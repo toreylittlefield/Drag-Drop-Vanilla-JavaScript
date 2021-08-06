f69faed82b6070f04d955807eba178b54084816a
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-08-06 07:14:18 -0400

    move createRipple to utils

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -24,4 +24,30 @@ const moveViewPortToCenter = (event) => {
   });
 };
 
-export { moveViewPortToCenter };
+/**
+ * @description creates the "ripple effect" for buttons
+ * @link https://css-tricks.com/how-to-recreate-the-ripple-effect-of-material-design-buttons/
+ * @param {Event} event
+ */
+const createRipple = (event) => {
+  const button = event.currentTarget;
+
+  const circle = document.createElement("span");
+  const diameter = Math.max(button.clientWidth, button.clientHeight);
+  const radius = diameter / 2;
+
+  circle.style.width = circle.style.height = `${diameter}px`;
+  circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+  circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+  circle.classList.add("ripple");
+
+  const ripple = button.getElementsByClassName("ripple")[0];
+
+  if (ripple) {
+    ripple.remove();
+  }
+
+  button.appendChild(circle);
+};
+
+export { moveViewPortToCenter, createRipple };
