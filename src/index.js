9782ce1502d7323b01e4539ef0cd42e691fc5008
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-08-08 21:51:45 -0400

    javascript for rotating the menu options around the circle

--- src/index.js
+++ src/index.js
@@ -25,3 +25,30 @@ import {
 scrollOnLoad();
 
 onWindowLoad(down);
+
+const btnFabSelector = document.querySelector(".btn.fab");
+console.log(btnFabSelector);
+const overlaySelector = document.querySelector(".overlay");
+const fablistSelector = document.querySelector(".fab-list");
+const fabListButtonsSelector = document.querySelectorAll(".fab-list button");
+btnFabSelector.onpointerdown = (event) => {
+  event.stopPropagation();
+  overlaySelector.classList.toggle("open");
+  const open = fablistSelector.classList.toggle("open");
+  let rotateAngle = 5;
+  let transitionTime = 300;
+  fabListButtonsSelector.forEach((button) => {
+    const transition = `transition: all ${transitionTime}ms ease ${transitionTime}ms`;
+    const translateXCSS = `translateX(-150%)`;
+    const cssText = `transform: rotate(${-rotateAngle}deg) ${translateXCSS}; ${transition};`;
+    if (open) {
+      button.parentElement.style.cssText = cssText;
+      button.style.cssText = `transform: rotate(${rotateAngle}deg); ${transition};`;
+    } else {
+      button.parentElement.style.cssText = `transform: rotate(0deg) translateX(0); ${transition};`;
+      button.style.cssText = `transform: rotate(0deg); ${transition};`;
+    }
+    rotateAngle -= 50;
+    transitionTime += 300;
+  });
+};
