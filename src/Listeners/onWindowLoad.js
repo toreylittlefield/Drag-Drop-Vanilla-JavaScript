463d957549124aab3369bf808a706e5fb1d751e6
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-08-09 00:15:56 -0400

    disable the dash-line pointer svg, move overlay

--- src/Listeners/onWindowLoad.js
+++ src/Listeners/onWindowLoad.js
@@ -23,23 +23,23 @@ export default (down = () => {}) => {
       (gridItem) => (gridItem.onpointerdown = down)
     );
 
-    window.onpointermove = (event) => {
-      const indicatorSelector = document.querySelector(
-        "svg#dashed-lines-svg.active-icon"
-      );
-      if (!indicatorSelector) {
-        const indicator = document.querySelector("svg#dashed-lines-svg");
-        indicator.classList.add("active-icon");
-      } else {
-        indicatorSelector.style.left =
-          parseFloat(event.pageX - indicatorSelector.clientWidth / 2) + "px";
-        indicatorSelector.style.top =
-          parseFloat(event.pageY - indicatorSelector.clientHeight / 2) + "px";
-        const closestElement = document.elementFromPoint(
-          parseInt(indicatorSelector.style.left, 10),
-          parseInt(indicatorSelector.style.top, 10)
-        );
-      }
-    };
+    // window.onpointermove = (event) => {
+    //   const indicatorSelector = document.querySelector(
+    //     "svg#dashed-lines-svg.active-icon"
+    //   );
+    //   if (!indicatorSelector) {
+    //     const indicator = document.querySelector("svg#dashed-lines-svg");
+    //     indicator.classList.add("active-icon");
+    //   } else {
+    //     indicatorSelector.style.left =
+    //       parseFloat(event.pageX - indicatorSelector.clientWidth / 2) + "px";
+    //     indicatorSelector.style.top =
+    //       parseFloat(event.pageY - indicatorSelector.clientHeight / 2) + "px";
+    //     const closestElement = document.elementFromPoint(
+    //       parseInt(indicatorSelector.style.left, 10),
+    //       parseInt(indicatorSelector.style.top, 10)
+    //     );
+    //   }
+    // };
   };
 };
